f4d0be20374780f21e833fc590b57fd8ae3ac08b
zddghost <2292011451@qq.com>
2019-10-31 15:04:46 +0800

    测试

---
+++ src/NioBuffer.java
@@ -0,0 +1,39 @@
+import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
+
+public class NioBuffer {
+
+
+
+    private static void readDemo() throws IOException {
+        RandomAccessFile aFile = new RandomAccessFile("e:/data/nio-data.txt", "rw");
+        FileChannel inChannel = aFile.getChannel();
+        //create buffer with capacity of 48 bytes
+        ByteBuffer buf = ByteBuffer.allocate(48);
+
+        int bytesRead = inChannel.read(buf); //read into buffer.
+        while (bytesRead != -1) {
+            buf.flip();  //make buffer ready for read
+            while (buf.hasRemaining()) {
+                System.out.print((char) buf.get()); // read 1 byte at a time
+            }
+            buf.clear(); //make buffer ready for writing
+            bytesRead = inChannel.read(buf);
+        }
+        aFile.close();
+    }
+
+    private static void readToWirte() throws Exception {
+        RandomAccessFile fromFile = new RandomAccessFile("e:/data/1.jpeg", "rw");
+        FileChannel fromChannel = fromFile.getChannel();
+
+        RandomAccessFile toFile = new RandomAccessFile("e:/data/2.jpeg", "rw");
+        FileChannel toChannel = toFile.getChannel();
+
+        long position = 0;
+        long count = fromChannel.size();
+        toChannel.transferFrom(fromChannel, position, count);
+    }
+}
